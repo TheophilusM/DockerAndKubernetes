150da3535291267912a4727573d4d0ee5f3c8207
TheophilusM <theotakumug@gmail.com>
2022-05-07 05:46:53 +0200

    node image

--- API/index.js
+++ API/index.js
@@ -9,3 +9,9 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+/*
+   npm init
+   npm i --save express
+   node index.js  
+*/
